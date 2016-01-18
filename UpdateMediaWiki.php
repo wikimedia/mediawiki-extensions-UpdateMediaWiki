bdd1eadbb837b087fb6aeab9117df7e211c59b34
Paladox <thomasmulhall410@yahoo.com>
2016-01-18 19:52:38 +0000

    Rename entry point file from updateMediaWiki to UpdateMediaWiki.php
    Change-Id: I8b7d732fdd1f0541ecdf58d124ac9e64d7ad6573

--- UpdateMediaWiki.php
+++ UpdateMediaWiki.php
@@ -12,24 +12,24 @@
 
 # Alert the user that this is not a valid access point to MediaWiki if they try to access the special pages file directly.
 if ( !defined( 'MEDIAWIKI' ) ) {
-        echo <<<EOT
+		echo <<<EOT
 To install updatemediawiki extension, put the following line in LocalSettings.php:
 require_once( "\$IP/extensions/updatemediawiki/updatemediawiki.php" );
 EOT;
-        exit( 1 );
+	exit( 1 );
 }
 
 $wgExtensionCredits['specialpage'][] = array(
-       'path' => __FILE__,
-       'name' => 'UpdateMediaWiki',
-       'author' =>'Miguel Peláez',
-       'url' => 'https://www.mediawiki.org/wiki/Extension:UpdateMediaWiki',
-       'descriptionmsg' => 'updatemediawiki-desc',
-       'version'  => '0.3.0',
+	'path' => __FILE__,
+	'name' => 'UpdateMediaWiki',
+	'author' =>'Miguel Peláez',
+	'url' => 'https://www.mediawiki.org/wiki/Extension:UpdateMediaWiki',
+	'descriptionmsg' => 'updatemediawiki-desc',
+	'version'  => '0.3.0',
 );
 
 $wgAutoloadClasses[ 'Specialupdatemediawiki' ] = __DIR__ . '/specialupdatemediawiki.php';
-$wgMessagesDirs['updatemediawiki'] = __DIR__ . '/i18n';
+$wgMessagesDirs['UpdateMediaWiki'] = __DIR__ . '/i18n';
 $wgExtensionMessagesFiles[ 'updatemediawiki' ] = __DIR__ . '/updatemediawiki.i18n.php';
 $wgSpecialPages[ 'updatemediawiki' ] = 'Specialupdatemediawiki';
-$wgExtensionMessagesFiles['updatemediawikiAlias'] = dirname( __FILE__ ) . '/updatemediawiki.alias.php';
+$wgExtensionMessagesFiles['updatemediawikiAlias'] = __DIR__ . '/updatemediawiki.alias.php';
