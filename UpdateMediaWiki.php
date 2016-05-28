6fcfd50f22740559e6d9674cbdd81325326c23e2
matejsuchanek <matejsuchanek97@gmail.com>
2016-05-28 12:44:52 +0200

    Convert to use extension registration
    Change-Id: I70141958c40d6269f7a828b31047fd20b9bfc5ed

--- UpdateMediaWiki.php
+++ UpdateMediaWiki.php
@@ -1,35 +1,16 @@
 <?php
 
-/*********************************************************************
-**
-** This file is part of the UpdateMediaWiki extension for MediaWiki
-* @file
-* @ingroup Extensions
-* @author Miguel Peláez <miguel2706outlook.com>
-* @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
-* @link http://www.mediawiki.org/wiki/Extension:UpdateMediaWiki Documentation
-**********************************************************************/
-
-# Alert the user that this is not a valid access point to MediaWiki if they try to access the special pages file directly.
-if ( !defined( 'MEDIAWIKI' ) ) {
-		echo <<<EOT
-To install updatemediawiki extension, put the following line in LocalSettings.php:
-require_once( "\$IP/extensions/updatemediawiki/updatemediawiki.php" );
-EOT;
-	exit( 1 );
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'UpdateMediaWiki' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['UpdateMediaWiki'] = __DIR__ . '/i18n';
+	$wgExtensionMessagesFiles['updatemediawikiAlias'] = __DIR__ . '/updatemediawiki.alias.php';
+	wfWarn(
+		'Deprecated PHP entry point used for FooBar extension. ' .
+		'Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
+	return;
+} else {
+	die( 'This version of the UpdateMediaWiki extension requires MediaWiki 1.25+' );
 }
-
-$wgExtensionCredits['specialpage'][] = array(
-	'path' => __FILE__,
-	'name' => 'UpdateMediaWiki',
-	'author' =>'Miguel Peláez',
-	'url' => 'https://www.mediawiki.org/wiki/Extension:UpdateMediaWiki',
-	'descriptionmsg' => 'updatemediawiki-desc',
-	'version'  => '0.3.0',
-);
-
-$wgAutoloadClasses[ 'Specialupdatemediawiki' ] = __DIR__ . '/specialupdatemediawiki.php';
-$wgMessagesDirs['UpdateMediaWiki'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles[ 'updatemediawiki' ] = __DIR__ . '/updatemediawiki.i18n.php';
-$wgSpecialPages[ 'updatemediawiki' ] = 'Specialupdatemediawiki';
-$wgExtensionMessagesFiles['updatemediawikiAlias'] = __DIR__ . '/updatemediawiki.alias.php';
