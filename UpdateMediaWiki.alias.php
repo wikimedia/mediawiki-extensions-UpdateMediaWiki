fca4d1a4b32d68b6234ee4374ed242ec50747235
MarcoAurelio <maurelio@tools.wmflabs.org>
2018-04-11 12:27:10 +0200

    Updates for the UpdateMediaWiki extension
     * Updated licensing headers to use SPDX-3.0 coding.
     * Updated licensing headers to use standard GNU licensing ones.
     * Apply MediaWiki codesniffer and PHPCS fixes.
     * Moved Special page to an /includes folder.
     * Updated version number in extension.json
     * Link extension author
    
    Change-Id: Ib1f13f221f4c44a6cd9c73cb657fe46d342cab16

--- UpdateMediaWiki.alias.php
+++ UpdateMediaWiki.alias.php
@@ -1,28 +1,39 @@
 <?php
-/*********************************************************************
-*
-* Aliases for UpdateMediaWiki
-*
-* This file is part of the UpdateMediaWiki extension for MediaWiki
-* @file
-* @ingroup Extensions
-* @author Miguel Peláez <miguel2706outlook.com>
-* @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
-* @link http://www.mediawiki.org/wiki/Extension:UpdateMediaWiki Documentation
-**********************************************************************/
-
-$specialPageAliases = array();
-
-/** English
- * @author Miguel2706
+/**
+ * Aliases for UpdateMediaWiki
+ *
+ * This file is part of the UpdateMediaWiki extension for MediaWiki
+ *
+ * Copyright (C) 2013, Miguel Peláez
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @file
+ * @ingroup Extensions
+ * @author Miguel Peláez <miguel2706outlook.com>
+ * @license http://www.gnu.org/copyleft/gpl.html GPL-2.0-or-later
+ * @link http://www.mediawiki.org/wiki/Extension:UpdateMediaWiki Documentation
  */
-$specialPageAliases[ 'en' ] = array(
-	'UpdateMediaWiki' => array( 'UpdateMediaWiki', 'Update MediaWiki' ),
-);
+$specialPageAliases = [];
 
-/** Spanish
- * @author Miguel2706
- */
-$specialPageAliases[ 'es' ] = array(
-	'UpdateMediaWiki' => array( 'ActualizarMediaWiki', 'Actualizar MediaWiki' ),
-);
+/** English (English) */
+$specialPageAliases[ 'en' ] = [
+	'UpdateMediaWiki' => [ 'UpdateMediaWiki', 'Update_MediaWiki' ],
+];
+
+/** Spanish (Español) */
+$specialPageAliases[ 'es' ] = [
+	'UpdateMediaWiki' => [ 'ActualizarMediaWiki', 'Actualizar_MediaWiki' ],
+];
--- includes/SpecialUpdateMediaWiki.php
+++ includes/SpecialUpdateMediaWiki.php
@@ -1,14 +1,29 @@
 <?php
-/*********************************************************************
-**
-** This file is part of the UpdateMediaWiki extension for MediaWiki
-* @file
-* @ingroup Extensions
-* @author Miguel PelÃ¡ez <miguel2706outlook.com>
-* @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
-* @link http://www.mediawiki.org/wiki/Extension:UpdateMediaWiki Documentation
-**********************************************************************/
-
+/*
+ * Copyright (C) 2013, Miguel Peláez
+ *
+ * This file is part of the UpdateMediaWiki extension.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @file
+ * @ingroup Extensions
+ * @author Miguel PelÃ¡ez <miguel2706outlook.com>
+ * @license http://www.gnu.org/copyleft/gpl.html GPL-2.0-or-later
+ * @link http://www.mediawiki.org/wiki/Extension:UpdateMediaWiki Documentation
+ */
 class SpecialUpdateMediaWiki extends SpecialPage {
 
 	function __construct() {
@@ -27,12 +42,12 @@ class SpecialUpdateMediaWiki extends SpecialPage {
 		$this->setHeaders();
 
 		global $wgVersion, $IP;
-		$getVersion = json_decode( file_get_contents( 'https://www.mediawiki.org/w/api.php?action=parse&format=json&text=%7B%7B%23invoke%3AVersion%7Cget%7Cstable%7Cversion%7D%7D&prop=text&disablelimitreport=1&disableeditsection=1&preview=1&disabletoc=1&contentmodel=wikitext' ), true ) or die ( 'ERROR' );
+		$getVersion = json_decode( file_get_contents( 'https://www.mediawiki.org/w/api.php?action=parse&format=json&text=%7B%7B%23invoke%3AVersion%7Cget%7Cstable%7Cversion%7D%7D&prop=text&disablelimitreport=1&disableeditsection=1&preview=1&disabletoc=1&contentmodel=wikitext' ), true ) or die( 'ERROR' );
 		$aV = $getVersion["parse"]["text"]["*"];
 		$updated = false;
 		$found = false;
 		if ( !empty( $aV ) ) {
-			//If we managed to access that file, then lets break up those release versions into an array.
+			// If we managed to access that file, then lets break up those release versions into an array.
 			$output->addWikiText( "== " . $this->msg( 'updatemediawiki-current', $wgVersion )->text() . " ==" );
 			$aV = str_replace( "<p>", "", $aV );
 			$aV = str_replace( "</p>", "", $aV );
@@ -41,14 +56,14 @@ class SpecialUpdateMediaWiki extends SpecialPage {
 			if ( $aV > $wgVersion ) {
 				$output->addWikiText( "<p>" . $this->msg( 'updatemediawiki-update-found', "v{$aV}" )->text() . "</p>" );
 				$found = true;
-				 //Download The File If We Do Not Have It
-				if ( !is_file( __DIR__ . "/Updates/mediawiki-{$aV}.tar.gz" ) ) {
+				 // Download The File If We Do Not Have It
+				if ( !is_file( __DIR__ . "/../Updates/mediawiki-{$aV}.tar.gz" ) ) {
 					$output->addWikiText( "<p>" . wfMessage( 'updatemediawiki-update-downloading' )->text() . "</p>" );
 					$newUpdate = file_get_contents( "http://download.wikimedia.org/mediawiki/{$mainVersion}/mediawiki-{$aV}.tar.gz" );
-					if ( !is_dir( __DIR__ . '/Updates' ) ) {
-						mkdir( __DIR__ . "/Updates", 0755 );
+					if ( !is_dir( __DIR__ . '/../Updates' ) ) {
+						mkdir( __DIR__ . "/../Updates", 0755 );
 					}
-					$dlHandler = fopen( __DIR__ . "/Updates/mediawiki-{$aV}.tar.gz", 'w');
+					$dlHandler = fopen( __DIR__ . "/../Updates/mediawiki-{$aV}.tar.gz", 'w' );
 					if ( !fwrite( $dlHandler, $newUpdate ) ) {
 						$output->addWikiText( "<p>" . $this->msg( 'updatemediawiki-update-aborted' )->text() . "</p>" );
 						exit();
@@ -60,20 +75,20 @@ class SpecialUpdateMediaWiki extends SpecialPage {
 				}
 
 				if ( $par == "doUpdate" ) {
-					//Open The File And Do Stuff
-					$gz = new PharData( "/Updates/mediawiki-{$aV}.tar.gz" );
+					// Open The File And Do Stuff
+					$gz = new PharData( "/../Updates/mediawiki-{$aV}.tar.gz" );
 					$output->addWikiText( "<ul>" );
 
-					//Make the directory if we need to...
-					if ( !is_dir( "/Updates{$aV}" ) ) {
-						mkdir( __DIR__ . "/Updates{$aV}", 0755 );
+					// Make the directory if we need to...
+					if ( !is_dir( __DIR__ . "/../Updates{$aV}" ) ) {
+						mkdir( __DIR__ . "/../Updates{$aV}", 0755 );
 					}
 
-					//Overwrite the file
+					// Overwrite the file
 
 					$gz = decompress(); // creates files.tar
 					// unarchive from the tar
-					$phar = new PharData( "Updates/mediawiki-{$aV}.tar" );
+					$phar = new PharData( "/../Updates/mediawiki-{$aV}.tar" );
 					$phar->extractTo( $IP );
 					$output->addWikiText( "<p>" . $this->msg( 'updatemediawiki-update-ready' )->text() . " [$IP/mw-config " . wfMessage( 'updatemediawiki-update-database' )->text() . "]</p>" );
 
